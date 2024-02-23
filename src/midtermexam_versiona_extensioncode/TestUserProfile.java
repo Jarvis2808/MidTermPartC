d226016553e10dcd6d3691a29c454e01e224ab72
Jarvis2808 <jainish716@gmail.com>
2024-02-23 16:17:22 -0500

    Second  Commit

---
+++ src/midtermexam_versiona_extensioncode/TestUserProfile.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+import java.util.Scanner;
+
+/**
+ *
+ * @author jaini
+ */
+public class TestUserProfile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        
+       
+        System.out.print("Enter your favorite actor: ");
+        String favoriteActor = scanner.nextLine();
+        
+       
+        UserProfile userProfile = new UserProfile(name, favoriteActor);
+        
+        
+        System.out.println("User profile created successfully for " + name);
+    }
+    
+}
